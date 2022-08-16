ad87c1134ce3eda34bec5fba979b8b27cf1eea85
David <96davitavetisyan@gmail.com>
2022-08-16 17:23:09 +0400

    qwknd

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div v-for = '(aaa,index) in article'>
+  <span>{{index + 1}}</span>
+    <span>{{aaa.name}}</span>
+    <span>{{aaa.email}}</span>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data(){
+    return{
+      article:'',
+    }
+  },
+  created() {
+    // Simple POST request with a JSON body using axios
+    const article = { title: "Vue POST Request Example" };
+    axios.post("http://127.0.0.1:8000/api/articles", article)
+      .then((response) => {
+        console.log(response)
+        this.article = response.data
+        console.log(this.article)
+      });
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
